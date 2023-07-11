8d840133e2d7191a7c37b73f3cb0494759df08b3
Md. Ismail <mdismailcse26@gmail.com>
2023-07-11 17:49:37 +0600

    added local language and localized storefront until 15.j2 j2 was completed  Book: Agile web development with rails 7

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
+    before_action :set_i18n_locale_from_params
+    # ...
     before_action :authorize
   
       # ...
@@ -6,8 +8,27 @@ class ApplicationController < ActionController::Base
     protected
   
       def authorize
-        unless User.find_by(id: session[:user_id])
-          redirect_to login_url, notice: "Please log in"
+        if [Mime[:html], Mime[:turbo_stream]].include? request.format
+          unless User.find_by(id: session[:user_id])
+            redirect_to login_url, notice: "Please log in"
+          end
+        else
+          authenticate_or_request_with_http_basic do |username, password|
+            user = User.find_by(name: username)
+            user && user.authenticate(password)
+          end
+        end
+      end
+  
+      def set_i18n_locale_from_params
+        if params[:locale]
+          if I18n.available_locales.map(&:to_s).include?(params[:locale])
+            I18n.locale = params[:locale]
+          else
+            flash.now[:notice] = 
+              "#{params[:locale]} translation not available"
+            logger.error flash.now[:notice]
+          end
         end
       end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -5,22 +5,18 @@ Rails.application.routes.draw do
     post 'login' => :create
     delete 'logout' => :destroy
   end
-
   get 'sessions/create'
   get 'sessions/destroy'
+
   resources :users
-  resources :orders
-  resources :line_items
-  resources :carts
-  root 'store#index', as: 'store_index'
   resources :products do
     get :who_bought, on: :member
   end
 
-  # Define your application routes per the DSL in
-  # https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  scope '(:locale)' do
+    resources :orders
+    resources :line_items
+    resources :carts
+    root 'store#index', as: 'store_index', via: :all
+  end
 end
-
